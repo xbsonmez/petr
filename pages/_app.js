68effa7e3e8f4c0e0a6dafa56f89b4ba71b2bf34
BURAK SONMEZ <42543196+xbsonmez@users.noreply.github.com>
2023-03-24 03:44:58 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,17 @@
 import '@/styles/globals.css';
 import CustomLayout from '../components/CustomLayout';
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
-  return (    
-  <CustomLayout>
-    <Component {...pageProps} />
-  </CustomLayout>)
+  return ( 
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"></meta>
+      </Head>
+      <CustomLayout>
+        <Component {...pageProps} />
+      </CustomLayout>)
+    </>
 
+  );
 }
