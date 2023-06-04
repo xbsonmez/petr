3b89d3b62c1a47feea2a6cbc8cd093cfc7de9a5b
BURAK SONMEZ <42543196+xbsonmez@users.noreply.github.com>
2023-06-05 01:40:59 +0300

    update

--- components/CustomVideoPlayer/index.js
+++ components/CustomVideoPlayer/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 
 
 
-export default function CustomVideoPlayer({source, ...rest}) {
+export default function CustomVideoPlayer({source, image,  ...rest}) {
 
     return (
         <div className="video-player">
-          <video className='videoClass' controls playsInline preload='auto'>
-                <source src={source} type="video/mp4" />
-            </video>
+          <video 
+          className='videoClass' 
+          controls 
+          playsInline 
+          preload='auto' 
+          src={source} 
+          type="video/mp4"
+          style={{backgroundImage:image}}
+          />
         </div>
     );
 }
--- components/Patience/index.js
+++ components/Patience/index.js
@@ -5,17 +5,17 @@ import styles from '@/styles/Home.module.css'
 
 const Patience = () =>{
 
-    const renderVideoSlider = [
-        <div key={1}><CustomVideoPlayer source={'/videos/ahmet.mp4'}/> </div>,
-        <div key={2}><CustomVideoPlayer source={'/videos/burak.mp4'}/> </div>,
-        <div key={3}><CustomVideoPlayer source={'/videos/emin.mp4'}/> </div>,
-        <div key={4}><CustomVideoPlayer source={'/videos/fatih.mp4'}/> </div>,
-        <div key={5}><CustomVideoPlayer source={'/videos/feridun.mp4'}/> </div>,
-        <div key={6}><CustomVideoPlayer source={'/videos/hakan.mp4'}/> </div>,
-        <div key={7}><CustomVideoPlayer source={'/videos/hasan.mp4'}/> </div>,
-        <div key={7}><CustomVideoPlayer source={'/videos/mustafa.mp4'}/> </div>,
-        <div key={7}><CustomVideoPlayer source={'/videos/osman.mp4'}/> </div>,
-        <div key={7}><CustomVideoPlayer source={'/videos/salim.mp4'}/> </div>,
+    const renderVideoSlider = [ 
+        <div key={1}><CustomVideoPlayer source={'/videos/ahmet.mp4'} image={'/before-after/ahmetbeforeafter.jpg'}/> </div>,
+        <div key={2}><CustomVideoPlayer source={'/videos/burak.mp4'} image={'/before-after/burak/burakbeforeafter.jpg'}/> </div>,
+        <div key={3}><CustomVideoPlayer source={'/videos/emin.mp4'} image={'/before-after/emin/eminbeforeafter.jpg'}/> </div>,
+        <div key={4}><CustomVideoPlayer source={'/videos/fatih.mp4'} image={'/before-after/fatih/fatihbeforeafter.jpg'}/> </div>,
+        <div key={5}><CustomVideoPlayer source={'/videos/feridun.mp4'} image={'/before-after/feridun/ferudun-beforeafter13.jpg'}/> </div>,
+        <div key={6}><CustomVideoPlayer source={'/videos/hakan.mp4'}  image={'/before-after/hakan/hakan-beforeafter14.jpg'}/> </div>,
+        <div key={7}><CustomVideoPlayer source={'/videos/hasan.mp4'} image={'/before-after/hasan/hasan-beforeafter20.jpg'}/> </div>,
+        <div key={7}><CustomVideoPlayer source={'/videos/mustafa.mp4'} image={'/before-after/bg.jpeg'}/> </div>,
+        <div key={7}><CustomVideoPlayer source={'/videos/osman.mp4'} image={'/before-after/osman/osman-beforeafter1.jpg'}/> </div>,
+        <div key={7}><CustomVideoPlayer source={'/videos/salim.mp4'}  image={'/before-after/saim/saimbeforeafter.jpg/'}/> </div>,
   
   ];
 
