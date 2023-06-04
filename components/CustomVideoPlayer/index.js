f94d7e70ee4bc8de15e77f931e0dd40bd5335810
BURAK SONMEZ <42543196+xbsonmez@users.noreply.github.com>
2023-06-05 00:41:55 +0300

    Update index.js

--- components/CustomVideoPlayer/index.js
+++ components/CustomVideoPlayer/index.js
@@ -6,7 +6,7 @@ export default function CustomVideoPlayer({source, ...rest}) {
 
     return (
         <div className="video-player">
-          <video width="100%" height="100%" controls>
+          <video width="100%" height="100%" controls playsInline>
                 <source src={source} type="video/mp4" />
             </video>
         </div>
